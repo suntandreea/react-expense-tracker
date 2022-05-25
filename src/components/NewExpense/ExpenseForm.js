70cb68880e7ec9992dd992346c837e17d2bb895c
Andreea Cozariuc <suntandreea@protonmail.com>
2022-05-25 16:15:07 +0300

    first functionalities

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,85 @@
+import React, {useState} from 'react';
+import "./ExpenseForm.css";
+
+const ExpenseForm = (props) => {
+
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
+    // alternativ, se poate utiliza un singur State, cu un obiect, insa
+    // trebuie avuta mare grija la cum se face actualizarea proprietarilor
+    // daca trimiti o singura proprietate, la update, fara sa faci ceva pt
+    // a le retine le celelalte, le vei pierde!!! 
+
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+    // });
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredTitle: event.target.value };
+        // });
+    };
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredAmount: event.target.value };
+        // });
+    };
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredDate: event.target.value };
+        // });
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault(); 
+        // ca sa nu mai faca refresh la pg si sa se piarda continutul formului
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+
+        props.onSaveExpenseData(expenseData);
+
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" value={enteredDate} min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
